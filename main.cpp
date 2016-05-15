6c1106a6a4dbac205aa0f4262101cca7635ddefd
Davidmue <d.mueller@ertl.ch>
2016-05-15 21:19:17 +0200

    The initial project

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Figure
+{
+public:
+  Figure() {
+    cout << "F ";
+  }
+};
+
+class Line : public virtual Figure
+{
+private:
+  double length;
+public:
+  Line(double len) : length(len) {
+    cout << "L ";
+  }
+};
+
+class Square : public virtual Figure
+{
+private:
+  double side;
+public:
+  Square(double side1) : side(side1) {
+    cout << "S ";
+  }
+};
+
+class Parallelepiped : public Square, public Line {
+public:
+  Parallelepiped(double baseSide, double height) :
+    Line(height), Square(baseSide) {
+    cout << "P ";
+  }
+};
+
+int main()
+{
+  Parallelepiped p(1,2);
+  return 0;
+}
+
